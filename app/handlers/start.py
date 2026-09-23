cbd9616e1b4527a15105a399cef2a2a0e082a1ce
zhengduo1539-code <zhengduo1539@gmail.com>
2026-09-23 13:18:34 +0630

    Localize customized welcome messages

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -6,7 +6,7 @@ from aiogram.types import Message
 
 from app.db.repository import DEFAULT_WELCOME, get_setting, list_welcome_buttons, upsert_user
 from app.keyboards.buttons import welcome_markup
-from app.services.emoji_service import deserialize_entities
+from app.services.emoji_service import deserialize_entities, localize_custom_emoji_entities
 from app.services.i18n import localized_default_welcome, normalize_language
 from app.services.number_converter import number_to_words
 
@@ -26,6 +26,9 @@ async def send_welcome(message: Message, session, bot, language: str = "en") -> 
         entities = []
     else:
         entities = deserialize_entities(await get_setting(session, "welcome_entities", "[]"))
+        if normalize_language(language) != "en":
+            localized_text = localized_default_welcome(language, number_to_words("10482", language))
+            text, entities = localize_custom_emoji_entities(text, entities, localized_text)
     buttons = await list_welcome_buttons(session)
     me = await bot.get_me()
     await message.answer(
