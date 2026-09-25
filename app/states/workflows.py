b58712efcb6584f613b77b9291a6372c15af5612
zhengduo1539-code <zhengduo1539@gmail.com>
2026-09-26 04:21:41 +0630

    Add configurable loading message workflow

--- app/states/workflows.py
+++ app/states/workflows.py
@@ -10,6 +10,10 @@ class LanguageMessageStates(StatesGroup):
     waiting_changed = State()
 
 
+class LoadingMessageStates(StatesGroup):
+    waiting_text = State()
+
+
 class RequestLanguageStates(StatesGroup):
     waiting_text = State()
 
